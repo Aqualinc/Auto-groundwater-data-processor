2860fa46926b99869934b70578eff88ce4e1b0c7
TimKerr <t.kerr@aqualinc.co.nz>
2019-07-17 11:03:10 +1200

    Added the baromeric correction function

--- GWProcessingFunctions.R
+++ GWProcessingFunctions.R
@@ -39,7 +39,7 @@ ReadXMLData <- function(XMLFile) {
 #' This function accepts a zoo timeseries of groundwater levels and a zoo timeseries of barometric pressure and adjusts the groundwater levels to account for atmospheric pressure variations
 #' @param GWSeries a timeseries object
 #' @param AirPressureSeries a timeseries object of air pressure values
-#' @value A timeseries object of corrected groundwater levels 
+#' @value A timeseries object of the groundwater levels after compensation for air pressure variability
 #' @keywords groundwater
 #' @export
 #' @examples
@@ -50,27 +50,29 @@ BarometricCorrection <- function(GWSeries, AirPressureSeries) {
   
   #Test for and load any libraries that are needed
   if (!require(zoo)) install.packages('zoo'); library(zoo)
-  
+  browser()
   #Check that the groundwater time series is wholly within the range of the barometric pressure series
   GWRange <- range(index(GWSeries))
   BaroRange <- range(index(AirPressureSeries))
-  if (GWRange[1] < BaroRange[1] | GWRange[2] > BaroRange[2]) stop('groundwater time series is not wholly within the range of the air pressure time series')
+  if (GWRange[1] < BaroRange[1] | GWRange[2] > BaroRange[2]) warning('groundwater time series is not wholly within the time range of the air pressure time series')
   
   #For every timestep in the groundwater series, find or estimate the barometric pressure. This is required in case the timestamps do not exactly allign, or if a different sampling frequency is used between the groundwater and air pressure series.
-  #Take the gw time series date-times and add it to the barometric series.
-  #Any duplicates, remove the ones without barometric pressure
-  #use the na.approx to fill the remainder.
-  AllignedAirPressure <- sapply(index(GWSeries), function(SingleDate) {
-    #sample the date of choice from the barometric pressure and interpolate
-    
-  })
+  GWAndBaro <- merge(GWSeries,AirPressureSeries)
+  GWAndBaro$AirPressureSeries <- na.approx(GWAndBaro$AirPressureSeries, method = "linear", na.rm = FALSE)
   
+  #Compensate the groundwater depths by subtracting the atmospheric pressure
+  Compensated <- with(GWAndBaro, GWSeries - AirPressureSeries)
+  
+  #Remove any NAs
+  Compensated <- Compensated[complete.cases(Compensated)]
+
+  return(Compensated)
 }
 
 
 #Load some sample data
 DataDirectory <- "G:\\ARL Projects\\WL Projects\\WL18036_EQC Earthquake Commission\\Data\\HighResolutionData\\FromT_T_January2019\\DH1 - DH4\\DH1-DH4 Raw\\Data Harvest 1\\Zone 1"
-BaroFile <- file.path(DataDirectory,"221_10_03_2017.xle")
+BaroFile <- file.path(DataDirectory,"221BARO_10_03_2017.xle")
 GroundwaterFile <- file.path(DataDirectory,"221_10_03_2017.xle")
 #221BARO_10_03_2017.xle
 #44_23_07_2017.xle
